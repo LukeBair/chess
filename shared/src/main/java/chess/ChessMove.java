accdf7388cd5a26a391cc1a3549659dab43bce40
LukeBair <74842241+LukeBair@users.noreply.github.com>
2025-07-16 18:49:09 -0600

    everythings fixed github wizardy

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -7,14 +9,13 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
-
-    private final ChessPosition start, end;
+    private final ChessPosition startPos, endPos;
     private final ChessPiece.PieceType promotionPiece;
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
-        this.start = startPosition;
-        end = endPosition;
+        this.startPos = startPosition;
+        this.endPos = endPosition;
         this.promotionPiece = promotionPiece;
     }
 
@@ -22,14 +23,14 @@ public class ChessMove {
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        return start;
+        return startPos;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        return end;
+        return endPos;
     }
 
     /**
@@ -40,5 +41,19 @@ public class ChessMove {
      */
     public ChessPiece.PieceType getPromotionPiece() {
         return promotionPiece;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj.getClass() != ChessMove.class) return false;
+
+        boolean samePos = ((ChessMove)obj).startPos.equals(startPos) && ((ChessMove)obj).endPos.equals(endPos);
+        boolean samePromo = (((ChessMove)obj).promotionPiece != null && promotionPiece != null && ((ChessMove)obj).promotionPiece == promotionPiece) || (((ChessMove)obj).promotionPiece == null && promotionPiece == null);
+        return samePos && samePromo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPos, endPos, promotionPiece);
     }
 }
