67bc0d57a68d6d928b68c9a9a0774f8ac36034ba
LukeBair <74842241+LukeBair@users.noreply.github.com>
2025-10-20 21:26:06 -0600

    memory data access class

---
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -0,0 +1,66 @@
+package dataaccess;
+import model.*;
+import java.util.*;
+
+public class MemoryDataAccess implements DataAccess {
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private final Map<String, AuthData> auths = new HashMap<>();
+    private int nextGameID = 1;
+
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+        games.clear();
+        auths.clear();
+        nextGameID = 1;
+    }
+    @Override
+    public void insertUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("User already exists");
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void createGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public List<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+    // Implement other methods similarly
+}
