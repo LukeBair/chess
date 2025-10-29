63bdf52d35640f4de36509e5f3b9c613343fde10
LukeBair <74842241+LukeBair@users.noreply.github.com>
2025-10-28 23:47:51 -0600

    stubs

---
+++ server/src/main/java/dataaccess/SQLDataAccess.java
@@ -0,0 +1,60 @@
+package dataaccess;
+
+import models.AuthData;
+import models.GameData;
+import models.UserData;
+
+import java.util.List;
+
+public class SQLDataAccess implements DataAccess {
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+
+    @Override
+    public void insertUser(UserData user) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void createGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public List<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+}
